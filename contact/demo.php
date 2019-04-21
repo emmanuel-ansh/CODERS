ec81995f254cc86c63c2ff39da3cdd3214da8c4b
ANSHUL TIGGA <35918337+emmanuel-ansh@users.noreply.github.com>
2019-04-21 21:53:38 +0530

    Add files via upload

---
+++ contact/demo.php
@@ -0,0 +1,44 @@
+<?php> <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="A hub for all the coders">
+    <meta name="keywords" content="code,program,solutions,java,c">
+    <meta name="author" content="ANSHUL TIGGA">
+    <title>CODER'S BAY|HOME</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div id="brand">
+          <h1><li><a href="index.php"><span class="high">CODER'S</span> BAY</a></l></h1>
+        </div>
+        <nav>
+          </ul>
+              <li><a href="index.php">Home</a></li>
+              <li><a href="about.html">About Us</a></li>
+              <li><a href="../contact/contact.php">Contact Us</a></li>
+              <li><a href="../sign/sign.php">Sign up</a></li>
+              <li><a href="../login/beg.php">Log in</a></li>
+            </ul>
+        </nav>
+
+
+    </header>
+      <section id="showcase">
+        <div class="container">
+        <div class="box">
+        <h1>Contact Information</h1>
+        <p>Phone no:7033540853</p>
+        <p>Phone no:7060639059</p>
+        <p>Email_id:anshulemmanuel@gmail.com</p>
+        </div>
+        </div>
+      </section>
+
+
+  </body>
+</html>
+<php?>
